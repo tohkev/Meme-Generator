6b370c97d8aed467eec730b62d92c7900e7d1c44
Kevin <tohkev@gmail.com>
2022-02-05 15:01:41 -0800

    added text-add functionality

--- src/components/Meme.js
+++ src/components/Meme.js
@@ -7,7 +7,7 @@ export default function Meme() {
     const [meme, setMeme] = React.useState({
         topText: "",
         bottomText: "",
-        randomImage: "https://i.imgflip.com/30b1gx.jpg"
+        randomImage: "https://i.imgflip.com/3si4.jpg"
     });
 
     function getNewMeme(event) {
@@ -22,6 +22,16 @@ export default function Meme() {
         })
     }
 
+    function handleChange(event) {
+        const { name, value } = event.target;
+        setMeme(prevMeme => {
+            return {
+                ...prevMeme,
+                [name]: value
+            }
+        })
+    }
+
     React.useEffect(() => {
         fetch('https://api.imgflip.com/get_memes')
             .then(res => res.json())
@@ -32,13 +42,27 @@ export default function Meme() {
         <div>
             <form className="meme--form">
                 <div className="meme--text-inputs">
-                    <input type="text" className="meme--text-input" placeholder="Top Text" />
-                    <input type="text" className="meme--text-input" placeholder="Bottom Text" />
+                    <input
+                        type="text"
+                        className="meme--text-input"
+                        placeholder="Top Text"
+                        name="topText"
+                        onChange={handleChange}
+                    />
+                    <input
+                        type="text"
+                        className="meme--text-input"
+                        placeholder="Bottom Text"
+                        name="bottomText"
+                        onChange={handleChange}
+                    />
                 </div>
                 <button className="meme--new-meme-btn" onClick={getNewMeme}>Get New Image 🖼</button>
             </form>
             <div className="meme--meme-block">
                 <img src={meme['randomImage']} alt="Meme Template" className="meme--template" />
+                <h2 className="meme--text top">{meme.topText}</h2>
+                <h2 className="meme--text bottom">{meme.bottomText}</h2>
             </div>
         </div>
     )
