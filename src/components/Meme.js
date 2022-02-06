5f0a5a7c5425da189acddc56be4ba4b17fdc40b7
Kevin <tohkev@gmail.com>
2022-02-05 16:06:55 -0800

    added fontawesome, reorg

--- src/components/Meme.js
+++ src/components/Meme.js
@@ -6,7 +6,6 @@ export default function Meme() {
 
     const [meme, setMeme] = React.useState({
         topText: "",
-        bottomText: "",
         randomImage: "https://i.imgflip.com/3si4.jpg"
     });
 
@@ -39,30 +38,34 @@ export default function Meme() {
     }, [])
 
     return (
-        <div>
-            <form className="meme--form">
-                <div className="meme--text-inputs">
-                    <input
-                        type="text"
-                        className="meme--text-input"
-                        placeholder="Top Text"
-                        name="topText"
-                        onChange={handleChange}
-                    />
-                    <input
-                        type="text"
-                        className="meme--text-input"
-                        placeholder="Bottom Text"
-                        name="bottomText"
-                        onChange={handleChange}
-                    />
+        <div className="meme--main">
+            <div className="meme--left">
+                <form className="meme--form">
+                    <div className="meme--text-inputs">
+                        <input
+                            type="text"
+                            className="meme--text-input"
+                            placeholder="Add Text"
+                            name="topText"
+                            onChange={handleChange}
+                        />
+                    </div>
+                    <div className="meme--options">
+                        <button className="meme--btn" onClick="#">Add Text</button>
+                        <button className="meme--btn new-image" onClick={getNewMeme}>Get New Image</button>
+                    </div>
+                </form>
+                <div className="meme--active-text">
+                    <h2>Current Texts:</h2>
+                    <ul className="meme--text-list">
+                        <li className="meme--text-item">What</li>
+                        <li className="meme--text-item">Huh</li>
+                    </ul>
                 </div>
-                <button className="meme--new-meme-btn" onClick={getNewMeme}>Get New Image 🖼</button>
-            </form>
-            <div className="meme--meme-block">
+            </div>
+            <div className="meme--right">
                 <img src={meme['randomImage']} alt="Meme Template" className="meme--template" />
-                <h2 className="meme--text top">{meme.topText}</h2>
-                <h2 className="meme--text bottom">{meme.bottomText}</h2>
+                <h2 className="meme--text top" draggable>{meme.topText}</h2>
             </div>
         </div>
     )
